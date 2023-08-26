11f87040c25473de19723505c3ecd62f4f4b7167
Farid D. Hamzayev <Farid.Hamzayev@kapitalbank.az>
2023-08-26 13:03:09 +0400

    Initial Commit

---
+++ clima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clima
+//
+//  Created by Farid Hamzayev on 26.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
